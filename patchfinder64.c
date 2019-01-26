a6a707a6965a79202d5cdfa6055d7304eabab341
Cryptiiiic <liamwqs@gmail.com>
2019-01-26 02:20:39 -0800

    Move find_boottime to correct location with credit

--- patchfinder64.c
+++ patchfinder64.c
@@ -1368,6 +1368,19 @@ addr_t find_strlen(void) {
     return offset_to_target + kerndumpbase;
 }
 
+
+/*
+ *
+ * 
+ *
+ */
+
+/*
+ *
+ * @Cryptiiiic's patches
+ *
+ */ 
+
 addr_t find_boottime(void) {
     addr_t ref = find_strref("%s WARNING: PMU offset is less then sys PMU", 1, string_base_oslstring);
     ref -= kerndumpbase;
@@ -1401,13 +1414,11 @@ addr_t find_boottime(void) {
     return val;
 }
 
-
 /*
  *
  * 
  *
  */
-
 
 
 #ifdef HAVE_MAIN
