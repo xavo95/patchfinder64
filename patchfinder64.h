7833d5155fa1567e2606e55761d0e449169ec689
Sam Bingner <sam@bingner.com>
2019-02-09 10:51:27 -1000

    Export find_fs_lookup_snapshot_metadata_by_name

--- patchfinder64.h
+++ patchfinder64.h
@@ -55,5 +55,6 @@ uint64_t find_chgproccnt(void);
 uint64_t find_kauth_cred_ref(void);
 uint64_t find_apfs_jhash_getvnode(void);
 uint64_t find_fs_lookup_snapshot_metadata_by_name_and_return_name(void);
+uint64_t find_fs_lookup_snapshot_metadata_by_name(void);
 
 #endif
